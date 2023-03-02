7794b0df8cef86b96f489ca73147689349b5c754
vah72 <hoangvananh987@Gmail.com>
2023-03-02 17:10:23 +0700

    chapter4

--- ecomstore/cart/views.py
+++ ecomstore/cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import RequestContext
-from cart import cart
+from . import cart
 
 
 def show_cart(request):
@@ -9,8 +9,9 @@ def show_cart(request):
         if postdata['submit'] == 'Remove': 
             cart.remove_from_cart(request) 
         if postdata['submit'] == 'Update': 
-            cart.update_cart(request) 
-    cart_items = cart.get_cart_items(request) 
+            cart.update_cart(request)
+    # cart_items = cart.get_cart_items(request) 
+    cart_items = cart.get_cart_items(request)
     page_title = 'Shopping Cart' 
     cart_subtotal = cart.cart_subtotal(request)
     return render(request, "cart/cart.html" , locals()) 
